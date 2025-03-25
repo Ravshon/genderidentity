26b1519855f217f86fe42d80f2c603b5d9c0568d
Ravshon <dunyoshow@mail.ru>
2025-03-25 17:53:01 +0500

    Add database connection script

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'gender_identity';
-$username = 'root'; // Измени при необходимости
-$password = '';
+$username = 'root'; // Username
+$password = ''; // Password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
