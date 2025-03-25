7338f025fb1bc950bc337ed0b3cea5ca2daf464c
Ravshon <dunyoshow@mail.ru>
2025-03-25 17:54:22 +0500

    1

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require 'db.php';
+
+    $first_name = trim($_POST['first_name']);
+    $last_name = trim($_POST['last_name']);
+
+    if (!empty($first_name) && !empty($last_name)) {
+        $stmt = $pdo->prepare("INSERT INTO users (first_name, last_name) VALUES (?, ?)");
+        $stmt->execute([$first_name, $last_name]);
+        $user_id = $pdo->lastInsertId();
+        header("Location: test.php?user_id=$user_id");
+        exit();
+    } else {
+        echo "Iltimos, ismingizni va familiyangizni kiriting!";
+    }
+}
+?>
